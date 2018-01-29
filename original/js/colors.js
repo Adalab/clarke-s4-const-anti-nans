82b251ac0cbbb020ea9e86f53f6df2aa050a543f
Icia_cr <icia83@gmail.com>
2018-01-29 08:44:01 +0100

    create js color

--- original/js/colors.js
+++ original/js/colors.js
@@ -1,42 +1,47 @@
 'use strict';
-var colors = document.querySelectorAll('.color_option');
-function cleanColorClasses() {
-    var previewLateral = document.querySelectorAll('.color-lateral');
-        for (var i = 0; i < previewLateral.length; i++) {
-        previewLateral[i].classList.remove('theme1', 'theme2', 'theme3', 'color-base');
-    }
-}
-function applyColorLateral(event) {
-    cleanColorClasses();
-    var previewLateral = document.querySelectorAll('.color-lateral');
-    var themeClass = event.currentTarget.getAttribute('data-theme-class');
-    for (var i = 0; i < previewLateral.length; i++) {
-        previewLateral[i].classList.add(themeClass);
-    }
-}
-for (var i = 0; i < colors.length; i++) {
-    colors[i].addEventListener('click', applyColorLateral);
+/*CONSTANTS DECLARATION*/
+const PREVIEW_LATERAL = document.querySelectorAll('.color-lateral');
+const PREVIEW_FONT = document.querySelectorAll('.type-font');
+
+/*VARIABLE DECLARATION*/
+let fonts = document.querySelectorAll('.font_option');
+let colors = document.querySelectorAll('.color_option');
+
+/*SELECT COLOR*/
+const cleanColorClasses = () => {
+	for (let cleanPreview of PREVIEW_LATERAL){
+		cleanPreview.classList.remove('theme1', 'theme2', 'theme3', 'color-base');
+	}
 }
 
-/*Seleccion fuentes*/
-var fonts = document.querySelectorAll('.font_option');
-function cleanFontClasses() {
-    var previewFont = document.querySelectorAll('.type-font');
-        for (var x = 0; x < previewFont.length; x++) {
-        previewFont[x].classList.remove('font1', 'font2', 'font3');
-    }
+const applyColorLateral = event => {
+	cleanColorClasses();
+	let themeClass = event.currentTarget.getAttribute('data-theme-class');
+	for (let applycolorPreview of PREVIEW_LATERAL){
+		applycolorPreview.classList.add(themeClass);
+	}
 }
-function applyFont(event) {
-    cleanFontClasses();
-    var previewFont = document.querySelectorAll('.type-font');
-    var fontClass = event.currentTarget.getAttribute('data-font-class');
-    for (var x = 0; x < previewFont.length; x++) {
-        previewFont[x].classList.add(fontClass);
-    }
+for (let iColor of colors){
+	iColor.addEventListener('click', applyColorLateral);
 }
 
-for (var x = 0; x < fonts.length; x++) {
-    fonts[x].addEventListener('click', applyFont);
+/*SELECT FONTS*/
+const cleanFontClasses = () =>{
+	for (let cleanFont of PREVIEW_FONT){
+		cleanFont.classList.remove('font1', 'font2', 'font3');
+	}
 }
+
+const applyFont = event =>{
+	cleanFontClasses();
+	let fontClass = event.currentTarget.getAttribute('data-font-class');
+	for (let applyFont of PREVIEW_FONT){
+		applyFont.classList.add(fontClass);
+	}
+}
+for (let iFonts of fonts){
+	iFonts.addEventListener('click', applyFont);
+}
+
 document.querySelector('.color_option').addEventListener('click', applyColorLateral);
 document.querySelector('.font_option').addEventListener('click', applyFont);
